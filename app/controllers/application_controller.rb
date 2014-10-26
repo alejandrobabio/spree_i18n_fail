f9c8690bc2063f6d8cbb188a701568670a12cb24
Alejandro Babio <alejandro.e.babio@hotmail.com>
2014-10-26 20:40:31 -0300

    make work i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :show_locales
+
+  def show_locales
+    puts I18n.locale
+    puts Spree.t :action
+  end
 end
